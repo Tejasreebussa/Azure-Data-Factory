2beec353474a73d437fb560eb85a3aabd34df547
Tejasree Bussa <tejasreeoct1026@gmail.com>
2024-02-09 12:21:40 -0800

    adding files_modified

--- etl_archive_files.py
+++ etl_archive_files.py
@@ -8,3 +8,22 @@ from pyspark.sql.functions import *
 source_folder_loc = "/mnt/datalake/UKG/Adf"
 archive_folder_loc = "/mnt/datalake/UKG/Adf_Archived"
 
+# COMMAND ----------
+
+def transform_load_files(source_path):
+  df = spark.read\
+            .format("csv")\
+            .option("header",True)\
+            .load(source_path)
+  
+  select_cols = ["Location","EPIC"]
+  table_name = source_path[source_path.rfind("/")+1:]
+  
+  transformed_df = df.select(*select_cols)\
+                     .withColumn("row_insert_tsp",lit(datetime.datetime.now()))
+    
+  transformed_df.write\
+                .format("delta")\
+                .saveAsTable(table_name)
+  
+
